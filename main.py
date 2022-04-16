ca7d71918a4b6afe1cb55c36ac4b348538ab1881
Shirinbek <shirinbekb@mail.ru>
2022-04-16 23:36:07 +0500

    knopkalar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+#from datetime import *
+
 import telebot
+from telebot import types
 from flask import Flask, request
 from config import *
 
@@ -11,11 +14,39 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Assalomu alaykum, ' + message.from_user.first_name)
 
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == "👋 Поздороваться"):
+        bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
+    elif(message.text == "❓ Задать вопрос"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Как меня зовут?")
+        btn2 = types.KeyboardButton("Что я могу?")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+
+    elif(message.text == "Как меня зовут?"):
+        bot.send_message(message.chat.id, "У меня нет имени..")
+
+    elif message.text == "Что я могу?":
+        bot.send_message(message.chat.id, text="Поздороваться с читателями")
+
+    elif (message.text == "Вернуться в главное меню"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("👋 Поздороваться")
+        button2 = types.KeyboardButton("❓ Задать вопрос")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, text="На такую комманду я не запрограммировал..")
+
+#bot.polling(none_stop=True)
+
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
     bot.reply_to(message, message.text)
-
 
 @server.route('/' + BOT_TOKEN, methods=['POST'])
 def get_message():
